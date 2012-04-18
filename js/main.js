7ed90291c47de27b67787fdbc956cde939154b8c
Willian Fernandes <willian@willianfernandes.com.br>
2012-04-18 20:55:19 -0300

    Changed the way to get Waypoints and now generate two Polygons for the same route

--- js/main.js
+++ js/main.js
@@ -4,38 +4,40 @@ HOTEL_COORDS = {
 };
 
 ATTRACTIONS = [
-  {
-    'latitude': -22.9865,
-    'longitude': -43.1884
-  },
-  {
-    'latitude': -22.9865,
-    'longitude': -43.2086
-  },
-  {
-    'latitude': -22.967,
-    'longitude': -43.1807
-  },
-  {
-    'latitude': -22.9722,
-    'longitude': -43.2122
-  },
-  {
-    'latitude': -22.9836,
-    'longitude': -43.2222
-  },
-  {
-    'latitude': -22.952,
-    'longitude': -43.1911
-  },
-  {
-    'latitude': -22.9545,
-    'longitude': -43.1674
-  },
-  {
-    'latitude': -22.9673,
-    'longitude': -43.2268
-  }
+  [
+    {
+      'latitude': -22.9865,
+      'longitude': -43.1884
+    },
+    {
+      'latitude': -22.9865,
+      'longitude': -43.2086
+    },
+    {
+      'latitude': -22.967,
+      'longitude': -43.1807
+    },
+    {
+      'latitude': -22.9722,
+      'longitude': -43.2122
+    },
+    {
+      'latitude': -22.9836,
+      'longitude': -43.2222
+    },
+    {
+      'latitude': -22.952,
+      'longitude': -43.1911
+    },
+    {
+      'latitude': -22.9545,
+      'longitude': -43.1674
+    },
+    {
+      'latitude': -22.9673,
+      'longitude': -43.2268
+    }
+  ]
 ];
 
 $(function() {
--- js/map.js
+++ js/map.js
@@ -1,16 +1,18 @@
-var Map                = {};
-var directionsService  = new google.maps.DirectionsService();
-var polygon_coords     = [];
-var waypts             = [];
+var Map                   = {};
+var directionsService     = new google.maps.DirectionsService();
+var polygons_coords       = [];
+var polygons              = [];
+var route_polygons        = [];
+var route_polygons_coords = [];
+var waypts                = [];
 var directionsDisplay;
 var map;
-var polygon;
 
 Map['initialize'] = function() {
   directionsDisplay = new google.maps.DirectionsRenderer();
   var hotel = new google.maps.LatLng(HOTEL_COORDS.latitude, HOTEL_COORDS.longitude);
   var myOptions = {
-    zoom: 12,
+    zoom: 13,
     mapTypeId: google.maps.MapTypeId.ROADMAP,
     center: hotel
   };
@@ -19,26 +21,95 @@ Map['initialize'] = function() {
   directionsDisplay.setMap(map);
 };
 
+Map['generatePolygon'] = function(route, color) {
+  var polygon_coords = [];
+
+  // Polygon coords
+  for (var i = 0; i < route.legs.length; i += 1) {
+    var leg              = route.legs[i];
+    var polygon_location = leg.start_location;
+    var marker_icon;
+
+    polygon_coords.push(polygon_location);
+
+    if (i == 0) {
+      marker_icon = 'img/hotel.png';
+    } else {
+      marker_icon = 'img/'+ i +'.png';
+    }
+
+    // Attraction marker
+    new google.maps.Marker({
+      position: polygon_location,
+      map: map,
+      icon: marker_icon
+    });
+  }
+  polygons_coords.push(polygon_coords);
+
+  // Polygons
+  polygons.push(new google.maps.Polygon({
+    paths: polygons_coords[0],
+    strokeColor: color,
+    strokeOpacity: 1,
+    strokeWeight: 3,
+    fillColor: color,
+    fillOpacity: 0
+  }));
+  polygons[0].setMap(map);
+};
+
+Map['generateRoutePolygon'] = function(route, color) {
+  var route_polygon_coords = [];
+  console.log(route);
+
+  for (var i = 0; i < route.legs.length; i += 1) {
+    var leg = route.legs[i];
+
+    for (var j = 0; j < leg.steps.length; j += 1) {
+      var step = leg.steps[j];
+
+      for (var k = 0; k < step.lat_lngs.length; k += 1) {
+        var lat_lng = step.lat_lngs[k];
+        route_polygon_coords.push(lat_lng);
+      }
+    }
+  }
+  route_polygons_coords.push(route_polygon_coords);
+
+  // Polygons
+  route_polygons.push(new google.maps.Polygon({
+    paths: route_polygons_coords[0],
+    strokeColor: color,
+    strokeOpacity: 1,
+    strokeWeight: 3,
+    fillColor: color,
+    fillOpacity: 0
+  }));
+  route_polygons[0].setMap(map);
+};
+
+Map['generateWaypoints'] = function() {
+  for (var i = 0; i < ATTRACTIONS.length; i++) {
+    var attractions = ATTRACTIONS[i];
+
+    for (var j = 0; j < attractions.length; j += 1) {
+      var attraction     = attractions[j];
+      var waypt_location = new google.maps.LatLng(attraction.latitude, attraction.longitude);
+
+      waypts.push({
+        location: waypt_location,
+        stopover:true
+      });
+    }
+  }
+};
+
 Map['traceRoute'] = function() {
   var start = new google.maps.LatLng(HOTEL_COORDS.latitude, HOTEL_COORDS.longitude);
   var end = new google.maps.LatLng(HOTEL_COORDS.latitude, HOTEL_COORDS.longitude);
 
-  // Hotel marker
-  new google.maps.Marker({
-    position: start,
-    map: map,
-    icon: 'img/hotel.png'
-  });
-
-  for (var i = 0; i < ATTRACTIONS.length; i++) {
-    var attractions = ATTRACTIONS[i];
-    waypt_location  = new google.maps.LatLng(attractions.latitude, attractions.longitude);
-
-    waypts.push({
-        location: waypt_location,
-        stopover:true
-    });
-  }
+  Map.generateWaypoints()
 
   var request = {
     origin:start,
@@ -54,31 +125,8 @@ Map['traceRoute'] = function() {
       var route = response.routes[0];
       var summaryPanel = document.getElementById('directions_panel');
 
-      // Polygon coords
-      for (var i = 0; i < route.optimized_waypoint_order.length; i += 1) {
-        var index            = route.optimized_waypoint_order[i];
-        var polygon_location = waypts[index].location;
-
-        polygon_coords.push(polygon_location);
-
-        // Attraction marker
-        new google.maps.Marker({
-          position: polygon_location,
-          map: map,
-          icon: 'img/'+ (i+1) +'.png'
-        });
-      }
-
-      // Polygon
-      polygon = new google.maps.Polygon({
-        paths: polygon_coords,
-        strokeColor: '#FF0000',
-        strokeOpacity: 1,
-        strokeWeight: 3,
-        fillColor: '#FF0000',
-        fillOpacity: 0
-      });
-      polygon.setMap(map);
+      Map.generatePolygon(route, '#FF0000');
+      Map.generateRoutePolygon(route, '#0000FF');
 
       summaryPanel.innerHTML = '';
       // For each route, display summary information.
@@ -88,7 +136,7 @@ Map['traceRoute'] = function() {
         } else {
           summaryPanel.innerHTML += '<b>Route Segment: ' + i + '</b><br />';
         }
-        summaryPanel.innerHTML += route.legs[i].start_address + ' to ';
+        summaryPanel.innerHTML += route.legs[i].start_address + ' até ';
         summaryPanel.innerHTML += route.legs[i].end_address + '<br />';
         summaryPanel.innerHTML += route.legs[i].distance.text + '<br /><br />';
       }
